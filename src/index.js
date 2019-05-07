e5460d30218d4d3d632033ce6fe7491eafe58673
Shaleen, Adam (ETW - FLEX) <Adam.Shaleen@nike.com>
2019-05-07 09:23:32 -0700

    added style and image bundling and image-loader to minify images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import _ from "lodash";
+import "./style.css";
+import Archer from "./archer_background.jpg";
 
-function component() {
-    const element = document.createElement('div');  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const component = () => {
+    const element = document.createElement("div");  
+    element.innerHTML = _.join(["Hello", "webpack"], " ");
+    element.classList.add("hello");
+
+    // line break
+    element.appendChild(document.createElement("br"));
+    
+    // add image
+    const archer = new Image();
+    archer.src = Archer;
+    archer.classList.add("image");
+    element.appendChild(archer);
   
     return element;
-  }
+}
   
-  document.body.appendChild(component());
+document.body.appendChild(component());
